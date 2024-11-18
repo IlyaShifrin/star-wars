81e10b2e52dc98277b48c9dba6995ea9a0541b4d
Ilya <drunktrumpet1@gmail.com>
2024-11-18 21:06:12 +0200

    Simple routing

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,23 +2,30 @@ import Home from "./Home.jsx";
 import AboutMe from "./AboutMe.jsx";
 import StarWars from "./StarWars.jsx";
 import Contact from "./Contact.jsx";
-import {navItems} from "../utils/constants.js";
-import {useContext} from "react";
-import {SWContext} from "../utils/context.js";
+import {Route, Routes} from "react-router-dom";
+import {navItems} from "../utils/constants.ts";
 
 const Main = () => {
-    const {page} = useContext(SWContext);
 
-    switch (page) {
-        case navItems[1]:
-            return <AboutMe/>;
-        case navItems[2]:
-            return <StarWars/>;
-        case navItems[3]:
-            return <Contact/>;
-        default:
-            return <Home/>;
-    }
+    return (
+        <Routes>
+            <Route path={'/'} element={<Home/>}/>
+            <Route path={`/${navItems[0]}`} element={<Home/>}/>
+            <Route path={`/${navItems[1]}`} element={<AboutMe/>}/>
+            <Route path={`/${navItems[2]}`} element={<StarWars/>}/>
+            <Route path={`/${navItems[3]}`} element={<Contact/>}/>
+        </Routes>
+    )
+    // switch (page) {
+    //     case navItems[1]:
+    //         return <AboutMe/>;
+    //     case navItems[2]:
+    //         return <StarWars/>;
+    //     case navItems[3]:
+    //         return <Contact/>;
+    //     default:
+    //         return <Home/>;
+    // }
 }
 
 export default Main;
